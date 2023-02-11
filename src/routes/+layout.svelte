aff9f5591af1cdbb13c0fce42049e28c5f2ddd82
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-02-11 17:17:33 +0900

    ✨ feat: impl logout
    '

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+{#if !$page.data.user}
+	<a href="/login">ログイン</a>
+	<a href="/register">新規登録</a>
+{:else}
+	<form action="/logout?/logout" method="POST">
+		<button>ログアウト</button>
+	</form>
+{/if}
+
+<slot />
---
+++ src/routes/logout/+page.server.ts
@@ -0,0 +1,14 @@
+import { redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async () => {
+	throw redirect(303, '/');
+};
+
+export const actions: Actions = {
+	logout: async ({ cookies }) => {
+		cookies.delete('session');
+
+		throw redirect(303, '/login');
+	}
+};
